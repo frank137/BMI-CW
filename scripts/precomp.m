ed6737ba40c9dbd79613877d05e5ea8734db3818
Francesco <francesco.guagliardo14@imperial.ac.uk>
2019-02-21 15:02:35 +0000

    started rastrer plot

--- scripts/precomp.m
+++ scripts/precomp.m
@@ -2,5 +2,19 @@
 clear, clc, close all
 load monkeydata_training.mat
 
+trial1 = [trial(1,1).spikes];
+
+[Units,T] = size(trial1); 
 
 
+surf(trial1)
+xlabel 'time (ms)'
+ylabel 'recording units'
+
+colormap(gray)
+colorbar
+
+figure('units','normalized','position',[.5 .5 .7 .4])
+trial1 = logical(trial1);
+plotSpikeRaster(trial1,'PlotType','imagesc','AutoLabel',true);
+ylabel 'Units'
