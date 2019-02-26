d7972ca49bdbe394ae117542e5204b47588d7ee9
Francesco <francesco.guagliardo14@imperial.ac.uk>
2019-02-26 11:30:13 +0000

    precom francesco update

---
+++ scripts/precomp_Franecsco.m
@@ -0,0 +1,31 @@
+%% Precomp FG LCR DO
+clear, clc, close all
+load monkeydata_training.mat
+
+trial1 = [trial(1,1).spikes];
+
+[Units,T] = size(trial1);
+
+
+surf(trial1)
+xlabel 'time (ms)'
+ylabel 'recording units'
+
+% colormap(gray)
+colorbar
+shading interp
+
+figure('units','normalized','position',[.5 .5 .7 .4])
+trial1 = logical(trial1);
+plotSpikeRaster(trial1,'PlotType','imagesc','AutoLabel',true);
+ylabel 'Units'
+
+figure
+plot(trial(1,1).handPos(1,:))
+hold on
+plot(trial(1,1).handPos(2,:))
+plot(trial(1,1).handPos(3,:))
+
+%% part 2
+
+
