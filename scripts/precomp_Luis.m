9d86e84989c25a4e5b4b4fc39fbb99896cdceccc
Luis Chaves <lc5415@ic.ac.uk>
2019-02-28 10:23:28 +0000

    ignore icloud

--- scripts/precomp_Luis.m
+++ scripts/precomp_Luis.m
@@ -26,12 +26,45 @@ ylabel 'Units'
 figure
 % hold on
 matrix = zeros(100,672);
-for i = 1:length(trial(:,1))
-        cell = trial(i,1).spikes(1,:);
+for i = 1:length(trial(:,1)) % for the 100 trials for movement 1
+        cell = trial(i,1).spikes(1,:); %pick spikes out of trial i for 
+        %electrode (neuron) 1 for all the timesteps
         plot(cell)
-%         pause(0.1)
+    pause(0.1)
 %         length(cell)
          matrix(i,1:length(cell)) = cell;
 end
 %   plotSpikeRaster(matrix)
+figure
   contour(matrix)
+  title('Spikes for one movement measured in one electrode over 100 trials')
+  ylabel('Trials')
+  xlabel('Time (ms)')
+  
+  %%
+  dt = 2; %delta t = 1ms, over which spike density will be evaluated
+  % for the 100 trials for movement 1
+  t = 1;
+%   spiketimes = zeros(100,30);
+  for i = 1:length(trial(:,1))
+      %for electrode(neuron) 1 and trial i for all the timesteos
+      cell = trial(i,1).spikes(1,:);
+      timelength = length(cell);
+%       spike_count = zeros(100,timelength);
+      
+%       spiketimes(i,1:length(find(cell))) = find(cell);
+%       psth(
+    spikes_total = spikes_total + cell;
+%       while t<timelength
+%           for t = t:t+dt
+%               spike_count(i,t) = sum(cell(t:t+dt));
+% %               t = t+dt;
+%           end
+%       end
+      
+  end
+
+
+% psth(
+  
+  
