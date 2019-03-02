b235b9cc512408f25026c0bbc742e1def05d4461
Luis Chaves <lc5415@ic.ac.uk>
2019-03-02 21:01:08 +0000

    PSTH function plotter
    This is dedidcated plotter function which plots PSTHs and takes as inputs a time window, the struct trial, movement of choice(1 to 8)...

---
+++ scripts/PSTHplotter.m
@@ -0,0 +1,99 @@
+function [] = PSTHplotter(trial,movement,window,plot_option,pause_in)
+%% Plot a peri-stimulus time histogram (psth) or spikes as spike desnity over time
+%movement number
+%movement = 3;
+%window = 300; %time window over which spikes will be avergaed
+
+%for all electrode do this
+for j = 1:96
+    %initialise total number of spikes
+    spikes_total = zeros(1,800);
+    
+    %initialise l2, later on length of cell2
+    l2 = 0;
+    
+    %for all trials and movement 1
+    for i = 1:100
+        %load spikes from electrode j for trial i and for selected movement
+        cell = trial(i,movement).spikes(j,:);
+        %get time length of spike measurement
+        timelength = length(cell);
+        %calculate remainder of timlength to visualising window
+        remainder = mod(timelength,window);
+        %pad cell with zeros so as to make cell divisible into an integer
+        %amount of time windows
+        cell = [cell,zeros(1,window-remainder)];
+        
+        %initialise index of cell2, w
+        w= 1;
+        %for length of cell and jumping in window steps
+        for i = 1:window:length(cell)
+            %cell2 = sum of elements of cell from index i to i+window-1,
+            %e.g. if window is equal to 5, from 1 to 5, 6 to 10...
+            cell2(w) = sum(cell(i:i+window-1));
+            %increase index of cell2 by 1
+            w = w+1;
+        end
+        
+        %save max length of cell2 for correct plotting later on
+        if length(cell2)>l2
+            l2 = length(cell2);
+        end
+        
+        %get difference between length of spikes_total and cell2
+        l_difference = length(spikes_total)-length(cell2);
+        %add current cell2 (padded with zeros on the right to spikes_total
+        spikes_total = spikes_total + [cell2,zeros(1,l_difference)];
+        %delete cell2 in case of any length mismatch between trials
+        clear cell2
+    end
+    
+    %spikes_total = spikes_total/100;
+    
+    %plot option 1 is an animation, so plots will be coming on top of each
+    %other for different electrodes
+    if plot_option == 1
+        %plot bar plot with x axis centered at the middle of each window, only
+        %plot correspondent spikes_total values as rest is 0s
+        bar(window*(1-0.5:l2-0.5),spikes_total(1:l2),1)
+        grid on
+        %if window is too short,only plot xticks in 20s as otherwise xaxis may
+        %be impossible to visualise
+        if window<20
+            xticks(20*(0:l2))
+        else
+            %if window is sufficiently large do ticks at every window edge
+            xticks(window*(0:l2))
+        end
+        
+        title({'PSTH for';['Movement ',num2str(movement),', Electrode ',num2str(j)];['with window of ',num2str(window),'ms']})
+        ylabel('Spike density (spikes/ms/trial)')
+        xlabel('Time(ms)')
+        
+        if isa(pause_in,'numeric')
+            pause(pause_in)
+        else
+            %default pause
+            pause(0.3)
+        end
+        
+    elseif plot_option == 2
+        subplot(16,6,j)
+        %plot bar plot with x axis centered at the middle of each window, only
+        %plot correspondent spikes_total values as rest is 0s
+        bar(window*(1-0.5:l2-0.5),spikes_total(1:l2),1)
+        grid on
+        %if window is too short,only plot xticks in 100s as otherwise xaxis may
+        %be impossible to visualise
+        if window<100
+            xticks(100*(0:l2))
+        else
+            %if window is sufficiently large do ticks at every window edge
+            xticks(window*(0:l2))
+        end
+        title(['Electrode ', num2str(j)])
+        suptitle(['PSTHs for movement ', num2str(movement),' with window of ',num2str(window),'ms'])
+    end
+end
+end
+
--- scripts/precomp_Luis.m
+++ scripts/precomp_Luis.m
@@ -29,7 +29,7 @@ matrix = zeros(100,672);
 for i = 1:length(trial(:,1)) % for the 100 trials for movement 1
     cell = trial(i,1).spikes(1,:); %pick spikes out of trial i for
     %electrode (neuron) 1 for all the timesteps
-%     plot(cell)
+    %     plot(cell)
     %     pause(0.1)
     %         length(cell)
     matrix(i,1:length(cell)) = cell;
@@ -43,52 +43,70 @@ xlabel('Time (ms)')
 
 %% Plot a peri-stimulus time histogram (psth) or spikes as spike desnity over time
 %movement number
-movement = 8;
+movement = 3;
+window = 300; %time window over which spikes will be avergaed
 %for all electrode do this
 for j = 1
-    spikes_total = zeros(1,750);
+    spikes_total = zeros(1,800);
     %for all trials and movement 1
+    l2 = 0;
     for i = 1:length(trial(:,movement))
         %for electrode(neuron) 1 and trial i for all the timesteos
         cell = trial(i,movement).spikes(j,:);
         timelength = length(cell);
-        l_difference = length(spikes_total)-length(cell);
-        spikes_total = spikes_total + [cell,zeros(1,l_difference)];
+        remainder = mod(timelength,window);
+        cell = [cell,zeros(1,window-remainder)];
         
+        
+        w= 1;
+        for i = 1:window:length(cell)
+            cell2(w) = sum(cell(i:i+window-1));
+            w = w+1;
+        end
+        
+        if length(cell2)>l2
+            l2 = length(cell2);
+        end
+        
+        l_difference = length(spikes_total)-length(cell2);
+        spikes_total = spikes_total + [cell2,zeros(1,l_difference)];
+        clear cell2
     end
     
-%     spikes_total = spikes_total/100;
+    %     spikes_total = spikes_total/100;
     % histogram(713,spikes_total);
     
     %plot(1:750,(spikes_total))
-    bar(spikes_total(:,:))
-title({['Movement ',num2str(movement),', Electrode ',num2str(j)];['with window of ',num2str(1),'ms']})
-ylabel('Spike density (spikes/ms/trial)')
-xlabel('Time(ms)')
+    bar(window*(1-0.5:l2-0.5),spikes_total(1:l2),1)
+    grid on
+    if window<20
+        xticks(20*(0:l2))
+    else
+        xticks(window*(0:l2))
+    end
+    ylim(window*[0 l2])
+    title({'PSTH for';['Movement ',num2str(movement),', Electrode ',num2str(j)];['with window of ',num2str(window),'ms']})
+    ylabel('Spike density (spikes/ms/trial)')
+    xlabel('Time(ms)')
     
     pause(0.3)
 end
 % psth(
-%% 
-j= 1;
-for i = 1:5:length(cell)
-cell2(j) = sum(cell(i:i+4));
-j = j+1
-i
-end
+%%
+
 
 
 %% Plot hand position for different trials
 %% total plotting
 hfig = figure('Toolbar','none',...
-              'Menubar', 'none',...
-              'Name','Tuning curves for all 96 electrodes',...
-              'NumberTitle','off',...
-              'IntegerHandle','off','units','normalized','outerposition',[0 0 1 1]);
+    'Menubar', 'none',...
+    'Name','Tuning curves for all 96 electrodes',...
+    'NumberTitle','off',...
+    'IntegerHandle','off','units','normalized','outerposition',[0 0 1 1]);
 movement = 3;
 for i = 1:100
     %     for movement = 1:8
-%     figure
+    %     figure
     handpos = trial(i,movement).handPos; %pick spikes out of trial i for
     %electrode (neuron) 1 for all the timesteps
     cell = trial(i,1).spikes;
@@ -138,7 +156,7 @@ for i = 1:100
     xlim([0 900])
     ylabel('Electrode index')
     
-         pause(0.05)
+    pause(0.05)
     %     end
 end
 
@@ -214,10 +232,10 @@ end
 
 %% tuning curve
 hfig = figure('Toolbar','none',...
-              'Menubar', 'none',...
-              'Name','Tuning curves for all 96 electrodes',...
-              'NumberTitle','off',...
-              'IntegerHandle','off');
+    'Menubar', 'none',...
+    'Name','Tuning curves for all 96 electrodes',...
+    'NumberTitle','off',...
+    'IntegerHandle','off');
 for j = 1:96
     spikes_total = zeros(8,975);
     %for all movements
@@ -246,23 +264,23 @@ for j = 1:96
     %calculate std of spikes over time
     std_spikes = std(spikes_total);
     %plot errorbar
-%     subplot(2,1,1)
-%     errorbar(avg_spikes,std_spikes)
-%     title(['Tuning curve for electrode ',num2str(j)])
-%     ylabel('Spike rate (# of spikes/ms/trial)')
-%     xlabel('Preferred direction')
-%     ylim([0 0.1])
-% xlim([1 8])
-%     subplot(2,1,2)
-%     plot(avg_spikes)
-%     title(['Tuning curve for electrode ',num2str(j)])
-%     ylabel('Spike rate (# of spikes/ms/trial)')
-%     xlabel('Preferred direction')
-%     ylim([0 0.1])
-% xlim([1 8])
-%     pause(0.2)
-
-subplot(10,10,j)
+    %     subplot(2,1,1)
+    %     errorbar(avg_spikes,std_spikes)
+    %     title(['Tuning curve for electrode ',num2str(j)])
+    %     ylabel('Spike rate (# of spikes/ms/trial)')
+    %     xlabel('Preferred direction')
+    %     ylim([0 0.1])
+    % xlim([1 8])
+    %     subplot(2,1,2)
+    %     plot(avg_spikes)
+    %     title(['Tuning curve for electrode ',num2str(j)])
+    %     ylabel('Spike rate (# of spikes/ms/trial)')
+    %     xlabel('Preferred direction')
+    %     ylim([0 0.1])
+    % xlim([1 8])
+    %     pause(0.2)
+    
+    subplot(10,10,j)
     errorbar(avg_spikes,std_spikes)
     title(num2str(j))
     ylim([0 0.1])
