79f0bad0dd6802e5aae5a70db1bef4881edcdd55
Luis Chaves <lc5415@ic.ac.uk>
2019-03-07 10:27:47 +0000

    SVM stuff

---
+++ scripts/SVMseparator.m
@@ -0,0 +1,19 @@
+%% SVM separator
+%This function will make up to 96 dimensions "plot"/matrix with the
+%electrodes PSTH (later on PSTH in the -100-300 ms window) and each
+%movement will have a different label
+%
+%
+%
+%
+%
+%
+%
+%
+%
+%
+%
+%
+%
+%
+%
